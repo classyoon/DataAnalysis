3d54dfa6e4450d19b8b8b50e2c645965db59b743
classyoon <96508736+classyoon@users.noreply.github.com>
2025-03-14 12:10:10 -0400

    Initial Commit

---
+++ DataAnalysis/DataAnalysisApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataAnalysisApp.swift
+//  DataAnalysis
+//
+//  Created by Conner Yoon on 3/14/25.
+//
+
+import SwiftUI
+
+@main
+struct DataAnalysisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DataAnalysisTests/DataAnalysisTests.swift
@@ -0,0 +1,17 @@
+//
+//  DataAnalysisTests.swift
+//  DataAnalysisTests
+//
+//  Created by Conner Yoon on 3/14/25.
+//
+
+import Testing
+@testable import DataAnalysis
+
+struct DataAnalysisTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
